21bbdce1dc9bf89792701df8dd1805d05617cb7d
hyechan90 <hyechan90@naver.com>
2019-11-12 18:13:45 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import MyName from './MyName';
-import Student from './Student';
-import Counter from './Counter';
-import TodoItem from './TodoItem';
+import React from "react";
+//import MyName from './MyName';
+//import Student from './Student';
+//import Counter from './Counter';
+import TodoItem from "./TodoItem";
+import styled from "styled-components";
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-left: 150px;
+  margin-right: 150px;
+`;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
-      value: '',
+      value: ""
     };
   }
 
@@ -21,18 +30,25 @@ class App extends React.Component {
     const items = this.state.items;
     items.push(this.state.value);
     this.setState({
-      items,
+      items
     });
   };
 
   render() {
     return (
       <>
-        <input type='text' onChange={this.onChange} />
-        <button onClick={this.onClickAdd}>추가</button>
-        {this.state.items.map((value, idx) => (
-          <TodoItem key={idx} value={value} />
-        ))}
+        <Div>
+          <input
+            type="text"
+            onChange={this.onChange}
+            style={({ border: "1px solid black" }, { height: "20px" })}
+          />
+          <button onClick={this.onClickAdd}>추가</button>
+
+          {this.state.items.map((value, idx) => (
+            <TodoItem key={idx} value={value} />
+          ))}
+        </Div>
       </>
     );
   }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,18 +1,25 @@
 // Counter.jsx
-import React, { Component } from 'react';
-import styled, { div } from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-self: center;
+`;
 
 class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: this.props.value,
-      show: true,
+      show: true
     };
   }
 
   onClickChange = e => {
-    const change = prompt('바꿀 값');
+    const change = prompt("바꿀 값");
     this.setState({ value: change });
   };
   onClickDelete = e => {
@@ -21,11 +28,11 @@ class TodoItem extends Component {
 
   render() {
     return (
-      <div style={{ display: this.state.show ? 'block' : 'none' }}>
+      <Div style={{ display: this.state.show ? "block" : "none" }}>
         <span>{this.state.value}</span>
         <button onClick={this.onClickChange}>수정</button>
         <button onClick={this.onClickDelete}>삭제</button>
-      </div>
+      </Div>
     );
   }
 }
